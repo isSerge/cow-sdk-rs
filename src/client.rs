d2732bead7c7cc1be05041c26870314654606b05
Serge Kovbasiuk <isserge@protonmail.com>
2025-03-20 20:42:53 +0700

    Add client module

---
+++ src/client.rs
@@ -0,0 +1,23 @@
+use crate::config::Config;
+use alloy::providers::{Provider, ProviderBuilder};
+use reqwest::Client as HttpClient;
+use reqwest::Url;
+use std::sync::Arc;
+
+pub struct Client {
+    config: Config,
+    http_client: Arc<HttpClient>,
+    provider: Arc<dyn Provider>,
+    // TODO: Add a signer
+}
+
+impl Client {
+    pub fn new(config: Config) -> Self {
+        let rpc_url = Url::parse(&config.rpc_url).unwrap();
+        Self {
+            config,
+            http_client: Arc::new(HttpClient::new()),
+            provider: Arc::new(ProviderBuilder::new().on_http(rpc_url)),
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+mod client;
 mod config;
 
 pub fn add(left: u64, right: u64) -> u64 {
